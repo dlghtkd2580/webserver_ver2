63e8d1e016c90d85c6228aac66365d7b6a4fe0e4
dlghtkd2580 <95616769+dlghtkd2580@users.noreply.github.com>
2025-01-16 16:09:34 +0900

    Update models.py
    add backup model

--- credit/web/models.py
+++ credit/web/models.py
@@ -106,6 +106,75 @@ class Form(models.Model):
         verbose_name = '신청서'
         verbose_name_plural = '신청서'
 
+class Backup(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    name = models.CharField(max_length=40, verbose_name="이름")
+
+    phone_number = models.CharField(max_length=40, verbose_name="휴대폰번호")
+
+    region = models.CharField(max_length=400, verbose_name="지역")
+
+    age = models.IntegerField(validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ], verbose_name="나이")
+
+    gender = models.CharField(max_length=40, choices=GENDER_CHOICES, verbose_name="성별")
+
+    job = models.CharField(null=True, blank=True, max_length=400, verbose_name="직업")
+
+    pay = models.IntegerField(validators=[
+            MaxValueValidator(100000000),
+            MinValueValidator(0)
+        ], verbose_name="급여(만원)")
+
+    pay_day = models.IntegerField(validators=[
+            MaxValueValidator(99),
+            MinValueValidator(1)
+        ], verbose_name="급여날짜(일)")
+
+    use_bool = models.CharField(max_length=40, choices=ANSWER_CHOICES, verbose_name="개인돈사용여부")
+    use_ps = models.TextField(max_length=4000, null=True, blank=True, verbose_name="개인돈사용내역")
+
+    credit_grade = models.IntegerField(validators=[
+            MaxValueValidator(9999),
+            MinValueValidator(0)
+        ], verbose_name="신용점수")
+
+    holding_credit = models.IntegerField(null=True, blank=True,validators=[
+            MaxValueValidator(1000000),
+            MinValueValidator(0)
+        ], verbose_name="기대출(만원)")
+
+    holding_house = models.CharField(max_length=40, choices=HOUSE_CHOICES, verbose_name="주거형태")
+
+    holding_car = models.CharField(max_length=40, choices=ANSWER_CHOICES, verbose_name="자차소유")
+
+    credit_amount = models.IntegerField(validators=[
+            MaxValueValidator(100000000),
+            MinValueValidator(1)
+        ], verbose_name="대출신청금(만원)")
+
+    credit_date = models.CharField(null=True,blank=True,max_length=40, choices=DATE_CHOICES, verbose_name="원하는상환방식")
+
+    is_married = models.CharField(max_length=40, choices=MARRY_CHOICES, verbose_name="결혼여부")
+
+    is_child = models.CharField(max_length=40, choices=CHILD_CHOICES, verbose_name="자녀여부")
+
+
+    def __str__(self):
+        return '(' + str(self.pk) + ') ' + str(self.date) + ' / ' +self.name + ' / ' + str(self.age) + '세 / ' + self.region
+
+    class Meta:
+        db_table = 'backups'
+        verbose_name = '백업'
+        verbose_name_plural = '백업'
+
+    
+
 class User(models.Model):
     user_id = models.CharField(unique=True, max_length=50, verbose_name="아이디")
     user_pw = models.CharField(max_length=200, verbose_name="비밀번호")
